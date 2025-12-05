219547888bad3ab20f7dd7b30fe71d165cd16df7
HuJhihSyun <cj654vmp@gmail.com>
2025-12-05 08:32:56 +0800

    [feat] banner 更換並新增對應元素

--- app/components/FeaturePanel.vue
+++ app/components/FeaturePanel.vue
@@ -1,24 +1,46 @@
 <script lang="ts" setup>
   import { Vue3Lottie } from 'vue3-lottie'
+  import featureSecurity from '@/assets/images/feature-security.json'
+  import featureAI from '@/assets/images/feature-ai2.json'
+  import featureBudget from '@/assets/images/feature-budget.json'
+
+  interface panelProps {
+    id: string
+  }
+  const props = withDefaults(defineProps<panelProps>(), {
+    id: ''
+  })
 </script>
 
 <template>
-  <div class="feature-panel px-4 pt-10 pb-4 bg-linear-to-b from-black/50 to-[#292d3a]/50 backdrop-blur-xs rounded-2xl">
-    <!-- <video src="@/assets/images/feature-ai.webm" width="100%" autoplay="true" controls="true"></video> -->
-    <!-- <img src="@/assets/images/banner.jpg" alt="" /> -->
-    <!-- <client-only>
-      <Vue3Lottie
-        class="data-section-content-image-content"
-        animationLink="https://hujhihsyun.github.io/SASE-Digital-Campaign/images/solution-computer.json"
-      />
-    </client-only> -->
-    <h3
-      class="text-xl font-semibold bg-clip-text text-transparent bg-linear-to-bl from-[#0070F8] to-[#62E5F6] HPEGraphikSemiBold"
-    >
-      <slot name="title" />
-    </h3>
-    <h4 class="text-xs text-[#62E5F6] HPEGraphikRegular"><slot name="subtitle" /></h4>
-    <p class="mt-2 text-base text-white HPEGraphikRegular"><slot name="description" /></p>
+  <div
+    class="feature-panel w-full px-6 xl:px-8 gap-4 xl:gap-6 bg-linear-to-b from-black/50 to-[#292d3a]/50 backdrop-blur-xs rounded-2xl flex justify-center items-center"
+  >
+    <div class="w-1/4 -translate-y-4">
+      <client-only>
+        <div v-if="id === 'security'" class="aspect-8/9">
+          <Vue3Lottie :animationData="featureSecurity" direction="forward" />
+        </div>
+        <div v-else-if="id === 'budget'" class="aspect-8/9">
+          <Vue3Lottie :animationData="featureBudget" direction="forward" />
+        </div>
+        <div v-else-if="id === 'ai'" class="aspect-8/9">
+          <Vue3Lottie :animationData="featureAI" direction="forward" />
+        </div>
+      </client-only>
+    </div>
+    <div class="w-full">
+      <h3
+        class="text-xl xl:text-2xl font-semibold bg-clip-text text-transparent bg-linear-to-bl from-[#0070F8] to-[#62E5F6] HPEGraphikSemiBold"
+      >
+        <slot name="title" /><span class="text-xs xl:text-sm text-[#62E5F6] HPEGraphikRegular ml-2"
+          ><slot name="subtitle"
+        /></span>
+      </h3>
+      <!-- <h4 class="text-xs text-[#62E5F6] HPEGraphikRegular"><slot name="subtitle" /></h4> -->
+      <h4 class="mt-1 text-base xl:text-lg text-white HPEGraphikRegular"><slot name="description" /></h4>
+      <p class="mt-2 xl:text-sm text-gray-300 HPEGraphikRegular"><slot name="content" /></p>
+    </div>
   </div>
 </template>
 
--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,27 +1,31 @@
 <script setup lang="ts">
+  import { Vue3Lottie } from 'vue3-lottie'
   import bannerBgLeft from '@/assets/images/banner-bg-left.svg?skipsvgo'
   import bannerBgRight from '@/assets/images/banner-bg-right.svg?skipsvgo'
+  import bannerContent from '@/assets/images/banner-content.svg?skipsvgo'
+  import bannerCircle from '@/assets/images/banner-circle.json'
 
   const featureData = [
     {
-      title: '資料管理混亂',
-      subtitle: 'Data',
-      description: '資料散落各處，治理效率下降'
-    },
-    {
+      id: 'security',
       title: '資安防護缺口',
       subtitle: 'Cyber Resilience',
-      description: '勒索病毒威脅加劇，沒有 NIST 2.0 架構造成現有防護措施漏洞'
+      description: '勒索病毒威脅加劇，沒有 NIST 2.0 架構造成現有防護措施漏洞',
+      content: '擔心勒索病毒攻擊、確保資料安全合規、快速災難復原'
     },
     {
+      id: 'budget',
       title: '虛擬化成本暴增',
       subtitle: 'VMware Alternative',
-      description: 'VMware 授權費暴漲，IT 預算吃緊難以為繼'
+      description: 'VMware 授權費暴漲，IT 預算吃緊難以為繼',
+      content: 'VMware 授權費暴漲、尋找高效能替代方案、控制總擁有成本'
     },
     {
+      id: 'ai',
       title: 'AI 效能瓶頸',
       subtitle: 'AI',
-      description: 'AI 專案進展緩慢，昂貴GPU使用率低落'
+      description: 'AI 專案進展緩慢，昂貴GPU使用率低落',
+      content: 'GPU 使用率低落、AI 訓練速度慢、資料讀取成為瓶頸'
     }
   ]
 </script>
@@ -29,22 +33,31 @@
 <template>
   <div>
     <header class="relative w-full h-[56vw] bg-linear-to-bl from-[#00E0AF] from-5% via-[#01A982] via-30% to-[#292D3A]">
-      <bannerBgRight class="absolute h-full right-0 top-1/2 -translate-y-1/2" />
-      <bannerBgLeft class="absolute h-full left-0 top-1/2 -translate-y-1/2" />
-      <div class="absolute w-full min-h-[13vw] bottom-0 left-0 bg-linear-to-t from-[#292D3A] to-transparent">
+      <bannerContent class="absolute w-1/2 top-[45%] right-1/20 -translate-y-1/2 z-20" />
+      <client-only>
+        <div class="absolute h-9/10 top-1/2 left-1/10 -translate-y-1/2 mix-blend-soft-light z-10">
+          <Vue3Lottie :animationData="bannerCircle" direction="alternate" />
+        </div>
+      </client-only>
+      <bannerBgRight class="absolute h-full right-0 top-1/2 -translate-y-1/2 mix-blend-color-dodge" />
+      <bannerBgLeft class="absolute h-full left-0 top-1/2 -translate-y-1/2 mix-blend-color-dodge" />
+
+      <div class="absolute w-full min-h-[13vw] bottom-0 left-0 bg-linear-to-t from-[#292D3A] to-transparent z-30">
         <div class="w-full h-full flex items-center justify-center">
-          <div class="w-3/10 2xl:w-1/4 h-full border border-white border-dashed"></div>
-          <div class="w-7/10 2xl:w-3/4 px-6 h-full border border-white border-dashed">
+          <div class="w-3/10 2xl:w-1/4 h-full"></div>
+          <div class="w-7/10 2xl:w-3/4 px-6 xl:px-12 h-full">
             <h1
-              class="text-white text-4xl lg:text-5xl 2xl:text-6xl font-bold mb-4 HPEGraphikBold leading-tight text-shadow-lg/30 tracking-wide"
+              class="text-white text-4xl lg:text-5xl 2xl:text-6xl font-bold mb-5 HPEGraphikBold leading-tight text-shadow-lg/30 tracking-wide"
             >
               企業數位轉型， <br />從解決 4 大儲存挑戰開始
             </h1>
-            <h2
-              class="mb-16 bg-clip-text text-transparent bg-linear-to-br from-[#00E0AF] to-[#62E5F6] to-70% text-xl lg:text-2xl 2xl:text-3xl font-semibold HPEGraphikSemiBold leading-tight text-shadow-lg tracking-wide"
+            <div
+              class="banner-heading mb-16 text-xl lg:text-3xl 2xl:text-4xl font-semibold HPEGraphikSemiBold leading-tight text-shadow-lg tracking-wide py-3 px-6 w-4/5 bg-linear-to-br from-[#0070F8]/20 via-black/20 to-black/0 rounded-2xl after:absolute after:content-[''] after:w-15 after:h-1 after:rounded-full after:bg-linear-to-r after:from-[#00E0AF] after:to-[#62E5F6] after:top-0 after:-translate-y-1/2 after:left-1/10"
             >
-              HPE Storage 全方位解決方案，讓您的 IT 基礎設施準備好迎接 AI 時代
-            </h2>
+              <h2 class="inline-block bg-clip-text text-transparent bg-linear-to-br from-[#00E0AF] to-[#62E5F6] to-70%">
+                HPE Storage 全方位解決方案，讓您的 IT 基礎設施準備好迎接 AI 時代
+              </h2>
+            </div>
           </div>
         </div>
       </div>
@@ -58,15 +71,20 @@
         <div class="w-3/10 2xl:w-1/4 h-full border border-white border-dashed"></div>
         <div class="w-7/10 2xl:w-3/4 h-full px-12 border border-white border-dashed">
           <MenuPanel />
-          <h2 class="mt-30 text-white text-xl lg:text-2xl 2xl:text-3xl font-bold">
-            這些資料管理挑戰，是不是讓您非常頭痛？
-          </h2>
-          <div class="grid grid-cols-2 lg:grid-cols-4 gap-4 mt-10">
-            <FeaturePanel v-for="(feature, index) in featureData" :key="index">
-              <template #title>{{ feature.title }}</template>
-              <template #subtitle>{{ feature.subtitle }}</template>
-              <template #description>{{ feature.description }}</template>
-            </FeaturePanel>
+          <div class="flex justify-center items-center my-15">
+            <h2
+              class="relative w-1/3 text-white text-xl lg:text-2xl 2xl:text-3xl font-bold text-shadow-lg/30 leading-normal before:absolute before:content-[''] before:w-1/4 before:h-1 before:bg-linear-to-br before:from-[#00E0AF] before:to-[#62E5F6] before:top-0 before:-translate-y-1/2 before:left-0 pt-5 backdrop-blur-[2px]"
+            >
+              這些資料管理挑戰，<br />是不是讓您非常頭痛？
+            </h2>
+            <div class="w-2/3 flex flex-col justify-center items-center gap-8 mt-10">
+              <FeaturePanel v-for="(feature, index) in featureData" :key="index" :id="feature.id">
+                <template #title>{{ feature.title }}</template>
+                <template #subtitle>{{ feature.subtitle }}</template>
+                <template #description>{{ feature.description }}</template>
+                <template #content>{{ feature.content }}</template>
+              </FeaturePanel>
+            </div>
           </div>
         </div>
       </div>
@@ -78,15 +96,29 @@
       <div class="relative w-full h-full flex items-center justify-center z-20 pointer-events-none">
         <div class="w-3/10 2xl:w-1/4 h-full border border-white border-dashed"></div>
         <div class="w-7/10 2xl:w-3/4 h-full px-12 border border-white border-dashed">
-          <h2 class="mt-30 text-white text-xl lg:text-2xl 2xl:text-3xl font-bold">
-            這些資料管理挑戰，是不是讓您非常頭痛？
-          </h2>
-          <div class="grid grid-cols-2 lg:grid-cols-4 gap-4 mt-10">
-            <FeaturePanel v-for="(feature, index) in featureData" :key="index">
-              <template #title>{{ feature.title }}</template>
-              <template #subtitle>{{ feature.subtitle }}</template>
-              <template #description>{{ feature.description }}</template>
-            </FeaturePanel>
+          <div class="flex justify-center items-start mt-20">
+            <div class="w-1/3">
+              <h4
+                class="relative pl-4 text-white text-lg lg:text-xl 2xl:text-2xl font-bold before:absolute before:content-[''] before:w-1 before:h-full before:bg-linear-to-br before:from-[#00E0AF] before:to-[#62E5F6] before:top-0 before:left-0 leading-none"
+              >
+                實戰工具箱
+                <span
+                  class="text-[#00E0AF] text-xs HPEGraphikRegular ml-1 border border-[#00E0AF] rounded-full px-2 py-0.5"
+                  >讓決策更簡單</span
+                >
+              </h4>
+            </div>
+            <div class="w-2/3">
+              <h4
+                class="relative pl-4 text-white text-lg lg:text-xl 2xl:text-2xl font-bold before:absolute before:content-[''] before:w-1 before:h-full before:bg-linear-to-br before:from-[#00E0AF] before:to-[#62E5F6] before:top-0 before:left-0 leading-none"
+              >
+                成功故事櫥窗
+                <span
+                  class="text-[#00E0AF] text-xs HPEGraphikRegular ml-1 border border-[#00E0AF] rounded-full px-2 py-0.5"
+                  >同行都在這樣做</span
+                >
+              </h4>
+            </div>
           </div>
         </div>
       </div>
@@ -97,3 +129,33 @@
     </ClientOnly>
   </div>
 </template>
+
+<style lang="scss" scoped>
+  .banner-heading {
+    position: relative;
+    outline: none;
+    border: none;
+
+    &:before {
+      position: absolute;
+      content: '';
+      bottom: 0;
+      top: 0;
+      left: 0;
+      right: 0;
+      border-radius: 16px;
+      background: linear-gradient(90deg, #00e0af, rgba(#00e0af, 0));
+      padding: 1px;
+
+      // gradient border with mask
+      mask:
+        linear-gradient(#fff 0 0) content-box,
+        linear-gradient(#fff 0 0);
+      -webkit-mask:
+        linear-gradient(#fff 0 0) content-box,
+        linear-gradient(#fff 0 0);
+      -webkit-mask-composite: xor;
+      mask-composite: exclude;
+    }
+  }
+</style>
